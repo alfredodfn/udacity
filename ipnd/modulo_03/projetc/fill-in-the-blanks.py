b33906c3a408df4c9ece7e9204a7f6b6ab3768ed
Alfredo Del Fabro Neto <alfredodfn@gmail.com>
2017-12-11 17:27:07 -0200

    Corrige problema de codificação para português brasil e mensagens no fluxo do jogo

--- ipnd/modulo_03/projetc/fill-in-the-blanks.py
+++ ipnd/modulo_03/projetc/fill-in-the-blanks.py
@@ -1,23 +1,30 @@
+# coding=utf-8
 LIVES = 5
 
-# PHRASES and LIST_OF_ANSWERS sao dependentes pelo indice
+# PHRASES e LIST_OF_ANSWERS sao relacionadas pelo indice da lista.
+# Ou seja, LIST_OF_ANSWERS[0] contém as respostas de PHRASES[0]
 PHRASES = [
-    "Essa eh ___1___ a primeira ___2___ frase ___3___ ",
+    "Essa eh ___1___ a primeira ___2___ frase ___3___ e ___4___",
     "Essa eh a segunda frase",
     "Essa eh a terceira frase",
 ]
 
+# Par identificador <-> valor que reflete a resposta para cada espaço
 LIST_OF_ANSWERS = [
-    [["___1___","Resposta"],["___2___","Resposta"],["___3___","Resposta"]],
-    [["___1___","Resposta"],["___2___","Resposta"],["___3___","Resposta"]],
-    [["___1___","Resposta"],["___2___","Resposta"],["___3___","Resposta"]]
+    [["___1___","Resposta"],["___2___","Resposta"],["___3___","Resposta"],
+        ["___4___","Resposta"]],
+    [["___1___","Resposta"],["___2___","Resposta"],["___3___","Resposta"],
+        ["___4___","Resposta"]],
+    [["___1___","Resposta"],["___2___","Resposta"],["___3___","Resposta"],
+        ["___3___","Resposta"]]
 ]
 
+# Imprime o Banner inicial
 def banner(lives):
-    print "Voce possui " + str(lives) + " tentativas!"
-    print "Informe o nivel de dificuldade. Pode ser: baixo, medio ou alto."
+    print "Você possui " + str(lives) + " tentativa(s)!"
+    print "Informe o nivel de dificuldade. Pode ser: fácil, médio ou difícil."
 
-# Inicializa o jogo
+# Inicializa o jogo com as vidas (tentativas máximas) e os numeros de acertos
 def init(lives):
     banner(lives)
     lives = lives
@@ -26,17 +33,17 @@ def init(lives):
 
 # Retorna o indice de uma frase baseado na dificuldade
 def select_phrase(level):
-    if (level == 'baixo'):
+    if (level == 'fácil'):
         return 0
-    elif (level == 'medio'):
+    elif (level == 'médio'):
         return 1
-    elif (level == 'alto'):
+    elif (level == 'difícil'):
         return 2
     # TODO: lancar uma exception caso nome nao suportado
     return None
 
-# Retorna uma lista com as chaves de
-# substituicao (e.g., ___1___) para uma determinada frase
+# Retorna uma lista com todas as chaves de substituicao (e.g., ___1___)
+# encontradas para uma determinada frase
 def get_replacement_keys(index, list_of_answers):
     keys = []
     i = 0
@@ -53,6 +60,14 @@ def check_answer(answer, key, list_of_answer):
             return True
     return False
 
+# Imprime o resultado do jogo baseado na quantidade de acertos
+def result(hits,max_hits):
+    if (hits == max_hits):
+        return "Você venceu!"
+    else:
+        return "Você não conseguiu acertar todos os desafios! Revise o conteúdo!"
+
+# Jogo em si, captura as informações do usuário e implementa as regras
 def play_game(lives,phrases,list_of_answers):
     lives,hits = init(LIVES)
     level = raw_input("Selecione o nivel de dificuldade: ")
@@ -61,10 +76,14 @@ def play_game(lives,phrases,list_of_answers):
     keys = get_replacement_keys(index, list_of_answers)
     max_hits = len(keys)
     while (lives > 0 and hits < max_hits):
-        answer = raw_input("Qual a palavra que substitui "+ keys[hits] + "? ")
+        answer = raw_input("Qual a palavra que substitui "+ keys[hits] + "? " +
+        "(tentativas restantes = "+str(lives)+") ")
         if (check_answer(answer, keys[hits], list_of_answers[index]) == False):
             lives -= 1
+            print "Tente novamente!"
         else:
+            print phrases[index]
             hits += 1
+    print result(hits,max_hits)
 
 play_game(LIVES,PHRASES,LIST_OF_ANSWERS)
