0249afc341c53ebeb0db09a0264d19e1ef6a770d
Alfredo Del Fabro Neto <alfredodfn@gmail.com>
2017-12-13 11:08:46 -0200

    Substitui estrutura de dados de lista para dicionário

--- ipnd/modulo_03/projetc/fill-in-the-blanks.py
+++ ipnd/modulo_03/projetc/fill-in-the-blanks.py
@@ -1,80 +1,58 @@
 # coding=utf-8
 
-LIVES = 5
-
-LEVEL_LIST = ['facil','medio','dificil']
-
-# PHRASES e LIST_OF_ANSWERS sao relacionadas pelo indice da lista.
-# Ou seja, LIST_OF_ANSWERS[0] contém as respostas de PHRASES[0]
-PHRASES = [
-    "A ___1___ (World Wide Web) é uma coleção de documentos ___2___ " +
-    "(HyperText Markup Language), o principal tipo de documento que nela pode " +
-    "ser contido. Entretanto, diversos outros tipos de arquivos são suportados: " +
-    "___3___, vídeos, músicas, textos planos, entre outros. A comunicação entre " +
-    "esses documentos é realizada por meio de ___4___.",
-
-    "A Web é composta pelo ___1___ (você, seu computador e um navegador), a ___2___ e os ___3___es que armazenam os documentos HTML. O ___1___, através do ___4___, interage com o ___3___ por meio do protocolo HTTP (Hypertext Transfer Protocol), requisitando os documentos HTML e demais tipos associados.",
-
-    "O ___1___ modifica o HTML alterando diretamente o elemento ou através de ___2___es. Os ___2___es são os nomes utilizados para filtrar os elementos atribuídos a uma ___3___ ou atributo de ___4___ (ID)."
-]
-
-# Par identificador <-> valor que reflete a resposta para cada espaço
-LIST_OF_ANSWERS = [
-    [["___1___","WWW"],["___2___","HTML"],["___3___","imagens"],
-        ["___4___","hyperlinks"]],
-    [["___1___","cliente"],["___2___","Internet"],["___3___","servidor"],
-        ["___4___","navegador"]],
-    [["___1___","CSS"],["___2___","seletor"],["___3___","classe"],
-        ["___4___","identificação"]]
-]
+# Estrutura de dados do jogo
+data = {
+    'facil': {
+        'phrase': "A ___1___ (World Wide Web) é uma coleção de documentos " + \
+         "___2___ (HyperText Markup Language), o principal tipo de documento " + \
+         "que nela pode ser contido. Entretanto, diversos outros tipos de " + \
+         "arquivos são suportados: ___3___, vídeos, músicas, textos planos, " + \
+         "entre outros. A comunicação entre esses documentos é realizada " + \
+         "por meio de ___4___.",
+        'answers': ['WWW','HTML','imagens','hyperlinks'],
+        'lives': 5
+    },
+    'medio': {
+        'phrase': "A Web é composta pelo ___1___ (você, seu computador e " + \
+         "um navegador), a ___2___ e os ___3___es que armazenam os documentos " + \
+         "HTML. O ___1___, através do ___4___, interage com o ___3___ por meio " + \
+         "do protocolo HTTP (Hypertext Transfer Protocol), requisitando os " + \
+         "documentos HTML e demais tipos associados.",
+        'answers': ['cliente','Internet','servidor','navegador'],
+        'lives': 2
+    },
+    'dificil': {
+        'phrase': "O ___1___ modifica o HTML alterando diretamente o elemento " + \
+        "ou através de ___2___es. Os ___2___es são os nomes utilizados para " + \
+        "filtrar os elementos atribuídos a uma ___3___ ou atributo de " + \
+        "___4___ (ID).",
+        'answers': ['CSS','seletor','classe','identificação'],
+        'lives': 1
+    }
+}
 
 # Imprime o Banner inicial
-def banner(lives):
-    print "Você possui " + str(lives) + " tentativa(s)!"
-    print "Informe o nivel de dificuldade. Pode ser: facil, medio ou dificil."
+def banner(level,data):
+    print "Você possui " + str(data[level]['lives']) + " tentativa(s)!"
 
 # Seleciona o nivel das perguntas a serem feitas ao usuario de acordo com
 # a lista de niveis permitidos level_list
-def select_level(level_list):
+def select_level(data):
     while True:
-        level = raw_input("Selecione o nivel de dificuldade: ")
-        if level in level_list:
+        level = raw_input("Selecione o nivel de dificuldade (facil, medio ou dificil): ")
+        if level in data:
             return level
         print "Nível \"" + level + "\" não suportado! Tente novamente."
 
 # Inicializa o jogo com as vidas (tentativas máximas) e os numeros de acertos
-def init_game(lives,level_list):
-    banner(lives)
-    lives = lives
+def init_game(data):
+    level = select_level(data)
+    banner(level,data)
+    lives = data[level]['lives']
     hits = 0
-    level = select_level(level_list)
-    return lives,hits,level
-
-# Retorna o indice de uma frase baseado na dificuldade
-def select_phrase_index(level, level_list):
-    return level_list.index(level)
-
-# Retorna uma lista com todas as chaves de substituicao (e.g., ___1___)
-# encontradas para uma determinada frase
-def get_replacement_keys(index, list_of_answers):
-    keys = []
-    kay_answer_index = 0
-    while ( kay_answer_index < len(list_of_answers[index])):
-        keys.append(list_of_answers[index][kay_answer_index][0])
-        kay_answer_index += 1
-    return keys
-
-# Verifica se a resposta esta presente na lista de respostas de uma
-# determinada pergunta
-def check_answer(answer, key, list_of_answer):
-    for la in list_of_answer:
-        if la[0] == key and la[1].lower() == answer.lower():
-            return True
-    return False
-
-# substitui a palavra key por answer em phrase
-def replace_answer(answer,key,phrase):
-    return phrase.replace(key,answer)
+    max_hits = len(data[level]['answers'])
+    phrase = data[level]['phrase']
+    return lives,hits,max_hits,level,phrase
 
 # Imprime o resultado do jogo baseado na quantidade de acertos
 def result(hits,max_hits):
@@ -84,22 +62,19 @@ def result(hits,max_hits):
         return "Você não conseguiu acertar todos os desafios! Revise o conteúdo!"
 
 # Jogo em si, captura as informações do usuário e implementa as regras
-def play_game(lives,phrases,list_of_answers,level_list):
-    lives,hits,level = init_game(LIVES,level_list)
-    index = select_phrase_index(level,level_list)
-    phrase = phrases[index]
-    keys = get_replacement_keys(index, list_of_answers)
-    max_hits = len(keys)
+def play_game(data):
+    lives,hits,max_hits,level,phrase = init_game(data)
     while (lives > 0 and hits < max_hits):
         print phrase
-        answer = raw_input("Qual a palavra que substitui "+ keys[hits] + "? " +
+        answer = raw_input("Qual a palavra que substitui ___"+ str(hits+1) + "___? " +
         "(tentativas restantes = "+str(lives)+") ")
-        if (check_answer(answer, keys[hits], list_of_answers[index]) == False):
-            lives -= 1
-            print "\nTente novamente!\n"
-        else:
-            phrase = replace_answer(answer,keys[hits],phrase)
+        if (answer.lower() == data[level]['answers'][hits].lower()):
+            print "\nCorreto!\n"
+            phrase = phrase.replace("___"+str(hits + 1)+"___", data[level]['answers'][hits])
             hits += 1
+        else:
+            print "\nResposta incorreta!\n"
+            lives -= 1
     print result(hits,max_hits)
 
-play_game(LIVES,PHRASES,LIST_OF_ANSWERS,LEVEL_LIST)
+play_game(data)
